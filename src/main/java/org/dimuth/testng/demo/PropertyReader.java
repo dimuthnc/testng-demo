12e8d50249c78d160019d0ca5be8cc9f2bf7b04d
Dimuth Menikgama <dimuth@wso2.com>
2020-08-31 20:35:37 +0530

    Improved PropertyReader class implementation

--- src/main/java/org/dimuth/testng/demo/PropertyReader.java
+++ src/main/java/org/dimuth/testng/demo/PropertyReader.java
@@ -5,31 +5,24 @@ import java.util.Properties;
 
 public class PropertyReader {
 
-    String configFileName;
+    Properties properties;
 
-    public PropertyReader(String configFileName){
-        this.configFileName = configFileName;
-    }
+    public PropertyReader(String configFileName) throws IOException {
 
-    public static Properties getProperties(String fileName) throws IOException {
+        this.properties.load(this.getClass().getClassLoader().getResourceAsStream(configFileName));
 
-        Properties properties = new Properties();
-        properties.load(PropertyReader.class.getClassLoader().getResourceAsStream(fileName));
-        return properties;
-
-    }
-
-    public static String getProperty(Properties properties, String key) {
-
-        return properties.getProperty(key);
     }
 
     public String[] getValues() throws IOException {
 
-        Properties properties = PropertyReader.getProperties(configFileName);
-        String value1 = PropertyReader.getProperty(properties, "value1");
-        String value2 = PropertyReader.getProperty(properties, "value2");
+        String value1 = readProperty("value1");
+        String value2 = readProperty("value2");
         return new String[]{value1, value2};
+    }
+
+    public String readProperty(String key) {
+
+        return properties.getProperty(key);
     }
 
 }
